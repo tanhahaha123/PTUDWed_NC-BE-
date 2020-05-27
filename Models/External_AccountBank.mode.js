886163e0d8761539ada7d8c084024e7a855dcc44
tanhahaha123 <tanhahaha123.mail@gmail.com>
2020-05-27 19:37:57 +0700

    Delete External_AccountBank.mode.js

--- Models/External_AccountBank.mode.js
+++
@@ -1,5 +0,0 @@
-const db=require('../Utils/database');
-module.exports={
-	getTaiKhoanThanhToan: STK => db.query("SELECT * FROM tai_khoan_thanh_toan WHERE SoTaiKhoanThanhToan = ?",[STK]),
-    checkBankPartner: BankName => db.query(`SELECT * FROM ngan_hang_liet_ket WHERE TenNganHang = '${BankName}'`)
-};
