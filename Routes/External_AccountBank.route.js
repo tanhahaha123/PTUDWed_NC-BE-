e0d53104f7f0847934be55a9b45b0ad6c024e7d5
tanhahaha123 <tanhahaha123.mail@gmail.com>
2020-06-02 23:37:52 +0700

    External_Acount edited

--- Routes/External_AccountBank.route.js
+++ Routes/External_AccountBank.route.js
@@ -1,16 +1,21 @@
+const fs = require('fs');
 const express = require('express');
 const request = require('request');
 const moment = require('moment');
+const md5 = require('md5');
+const NodeRSA = require('node-rsa');
 
 const config = require('../Config/config.json');
 const External_AccountBankModel = require('../Models/External_AccountBank.model');
 
+const MyBank_PrivateKey = fs.readFileSync('Keys/RSA/MyPrivateKey.pem','utf8');
+const MyBank_PrivateKeyObject = new NodeRSA(MyBank_PrivateKey);
 
 const router = express.Router();
 
 //Truy vấn thông tin tài khoản của ngân hàng khác
 router.get('/destination-account', async (req, res) => {
-    let payload = req.MyPayload;
+    let payload = req.body;
 
     // payload = {
     //     "DestinationBankName":".....",
@@ -22,38 +27,47 @@ router.get('/destination-account', async (req, res) => {
 
     if (DestinationBankName == null){
         return res.status(400).json({
-            err: 'Vui lòng kiểm tra lại trường DestinationBankName'
+            err: 'Vui lòng kiểm tra lại trường DestinationBankName, sai định dạng hoặc đang bỏ trống'
         });     
     }
     if (isNaN(DestinationAccountNumber) || (DestinationAccountNumber == null)){
         return res.status(400).json({
-            err: 'Vui lòng kiểm tra lại trường DestinationAccountNumber'
+            err: 'Vui lòng kiểm tra lại trường DestinationAccountNumber, sai định dạng hoặc đang bỏ trống'
         });     
     }
 
-    let data = await Partner_AccountBankModel.checkBankPartner(DestinationBankName);
+    let data = await External_AccountBankModel.checkBankPartner(DestinationBankName);
     if (data.length === 0) return res.status(406).json({"reply":"Xin lỗi, không tìm thấy ngân hàng liên kết trong cơ sở dữ liệu"});
 
     DestinationAccountNumber = +DestinationAccountNumber;
 
     switch (DestinationBankName) {
-        case "AGRIBANK":
+        case "GO":
         {
-            request('http://www.google.com', function (error, response, body) {
-              console.error('error:', error); // Print the error if one occurred
-              console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-              console.log('body:', body); // Print the HTML for the Google homepage.
-              res.json(body);
-            });
-            break;
-        }
-        case "VIETCOMBANK":
-        {
-            request('http://www.google.com', function (error, response, body) {
-              console.error('error:', error); // Print the error if one occurred
-              console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-              console.log('body:', body); // Print the HTML for the Google homepage.
-              res.json(body);
+            const ts = moment().valueOf();
+            const hashSecretKey = md5("Infymt");
+            const reqBody = {"account_number":DestinationAccountNumber.toString()};
+            request({
+                method: 'GET',
+                url: 'https://infymt.herokuapp.com/api/accounts/partner',
+                headers: {
+                    "partnerCode":"25Bank",
+                    "ts": ts,
+                    "sig": md5(ts + JSON.stringify(reqBody) + hashSecretKey)
+                },
+                json: reqBody
+            }, function (error, response, body) {
+                if (error) return res.status(500).json({"err":"Vui lòng thử lại request"});
+
+                try {
+                    body = JSON.parse(body);
+                } catch(err){
+                    //ignore
+                }
+
+                if ((response.statusCode === 500)&&(body.message==="Error.")) return res.status(204).json({});
+
+                res.json(body);
             });
             break;
         }
@@ -62,19 +76,21 @@ router.get('/destination-account', async (req, res) => {
 
 //Nạp tiền vào tài khoản ngân hàng khác
 router.post('/destination-account/recharge', async (req, res) => {
-    let payload = req.MyPayload;
+    let payload = req.body;
 
     // payload = {
     //     "DestinationBankName":"AGRIBANK",
     //     "SourceAccountNumber":258258258,
+    //     "SourceAccountName":"Đặng Văn Lâm",
     //     "DestinationAccountNumber":102102102,
+    //     "DestinationAccountName":"Nguyễn Thanh Nam",
     //     "Amount":1250000, //1 triệu 2 trăm 50 nghìn
     //     "Message":"thanh toán tiền phòng",
     // }
 
     if (payload.DestinationBankName == null){
         return res.status(400).json({
-            err: 'Vui lòng kiểm tra lại trường DestinationBankName, không được bỏ trống'
+            err: 'Vui lòng kiểm tra lại trường DestinationBankName, sai định dạng hoặc đang bỏ trống'
         });     
     }
 
@@ -84,15 +100,21 @@ router.post('/destination-account/recharge', async (req, res) => {
         });
     }
 
-    if (payload.SourceAccountName == null){
+    if (payload.SourceAccountName == null) {
         return res.status(400).json({
-            err: 'Vui lòng kiểm tra lại SourceAccountName, không được bỏ trống'
-        });     
+            err: 'Vui lòng kiểm tra lại trường SourceAccountName, sai định dạng hoặc đang bỏ trống'
+        });
     }
 
     if (isNaN(payload.DestinationAccountNumber) || (payload.DestinationAccountNumber == null)) {
         return res.status(400).json({
             err: 'Vui lòng kiểm tra lại trường DestinationAccountNumber, sai định dạng hoặc đang bỏ trống'
+        });
+    }
+
+    if (payload.DestinationAccountName == null) {
+        return res.status(400).json({
+            err: 'Vui lòng kiểm tra lại trường DestinationAccountName, sai định dạng hoặc đang bỏ trống'
         });
     }
 
@@ -104,11 +126,11 @@ router.post('/destination-account/recharge', async (req, res) => {
 
     if (payload.Message == null){
         return res.status(400).json({
-            err: 'Vui lòng kiểm tra lại trường Message, không được bỏ trống'
+            err: 'Vui lòng kiểm tra lại trường Message, sai định dạng hoặc đang bỏ trống'
         });     
     }
 
-    let data = await External_AccountBankModel.checkBankPartner(DestinationBankName);
+    let data = await External_AccountBankModel.checkBankPartner(payload.DestinationBankName);
     if (data.length === 0) return res.status(406).json({"reply":"Xin lỗi, không tìm thấy ngân hàng liên kết trong cơ sở dữ liệu"});
 
     //parse from string to number
@@ -126,28 +148,61 @@ router.post('/destination-account/recharge', async (req, res) => {
     let SoDuHienTai = TaiKhoanThanhToanNguon[0].SoDu;
     if ((SoDuHienTai-config.BANK.MinimumBalance) < payload.Amount) { //MinimumBalance xác định trong tài khoản phải có ít nhất 50 nghìn đồng và không được giao dịch với số tiền này
         return res.status(406).json({
-            err: 'Xin lỗi, số dư không đủ để thực hiện giao dịch này'
+            err: 'Xin lỗi, số dư không đủ để thực hiện giao dịch này, sau giao dịch phải có ít nhất 50.000 đồng',
+            CurrentBalance: SoDuHienTai
         })
     }
 
-    switch (DestinationBankName) {
-        case "AGRIBANK":
+    switch (payload.DestinationBankName) {
+        case "GO":
         {
-            request('http://www.google.com', function (error, response, body) {
-              console.error('error:', error); // Print the error if one occurred
-              console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-              console.log('body:', body); // Print the HTML for the Google homepage.
-              res.json(body);
-            });
-            break;
-        }
-        case "VIETCOMBANK":
-        {
-            request('http://www.google.com', function (error, response, body) {
-              console.error('error:', error); // Print the error if one occurred
-              console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-              console.log('body:', body); // Print the HTML for the Google homepage.
-              res.json(body);
+            const ts = moment().valueOf();
+            const hashSecretKey = md5("Infymt");
+            const reqBody = {"account_number":payload.DestinationAccountNumber.toString(),"money":payload.Amount,"currentTime":ts};
+            const signature = MyBank_PrivateKeyObject.sign(reqBody,"base64");
+            request({
+                method: 'POST',
+                url: 'https://infymt.herokuapp.com/api/accounts/partner/recharge',
+                headers: {
+                    "partnerCode":"25Bank",
+                    "ts": ts,
+                    "sig": md5(ts + JSON.stringify(reqBody) + hashSecretKey),
+                    "signature": signature
+                },
+                json: reqBody
+            }, async function(error, response, body) {
+                if (error) return res.status(500).json({"err":"Vui lòng thử lại request"});
+
+                try {
+                    body = JSON.parse(body);
+                } catch(err){
+                    //ignore
+                }
+
+                if ((response.statusCode === 200)&&(body==="Number not found")) return res.status(400).json({err: 'DestinationAccountNumber not found'});
+
+                res.json(body);
+
+                let resultUpdate = await External_AccountBankModel.updateSoDu(payload.SourceAccountNumber,SoDuHienTai-payload.Amount);
+                if (resultUpdate.affectedRows===0) return res.status(503).json({
+                    err: 'Service Unavailable'
+                });
+
+                let rowLichSuGiaoDich = {
+                    "MaGiaoDich": payload.SourceAccountNumber+'_'+ts,
+                    "SoTaiKhoanGiaoDich": payload.SourceAccountNumber,
+                    "NgayGiaoDich": moment().format("YYYY-MM-DD HH:mm:ss.SSS"), //thời điểm hiện tại
+                    "SoTien": payload.Amount,
+                    "NoiDung": payload.Message,
+                    "SoTaiKhoanNguoiGui": payload.DestinationAccountNumber,
+                    "ThongTinNguoiGui": payload.DestinationAccountName,
+                    "LienNganHang": 1,
+                    "TenNganHang": payload.DestinationBankName,
+                    "LoaiGiaoDich": "chuyển khoản"
+                }
+
+                let resultAdd = await External_AccountBankModel.addLichSuGiaoDich(rowLichSuGiaoDich);
+                if (resultAdd.affectedRows===0) throw new Error("Khong them duoc lich su giao dich, MaGiaoDich = "+rowLichSuGiaoDich.MaGiaoDich);
             });
             break;
         }
